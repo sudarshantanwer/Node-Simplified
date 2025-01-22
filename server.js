60d58929d241badf9dc1565f78bf2e8ab549c8e4
tanwarsud <sudarshan.tanwar2@tabcorp.com.au>
2025-01-22 21:19:17 +0530

    cors added

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin : 'http://localhost:3001'
+}))
 
 // In-memory database (mock)
 let todo = [
