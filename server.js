ac3032ca752fcedc2e2991b2de40b5e8073c18b8
tanwarsud <sudarshan.tanwar2@tabcorp.com.au>
2025-03-11 16:52:25 +0530

    Small changes in format

--- server.js
+++ server.js
@@ -69,12 +69,10 @@ const createNewCsv = (inputFilePath, outputFilePath) => {
 
   parser.on('data', (row) => {
     const newRow = {
-      'Summary': (row['Key']?.trim() || '') + '-' + row['Summary']?.trim() || '',
+      'Summary': (row['Key']?.trim() || '') + ' | ' + row['Summary']?.trim() || '',
       'Components': getComponentsData(row),
       'Priority': row['Priority']?.trim() || '',
       'Description': 'https://astrogo.atlassian.net/browse/' + row['Key'],
-      'Assignee': 'Unassigned',
-      'Reporter': 'Astro',
       'Status': 'Open',
       'Triage_Status': 'Triaged',
       'Affects versions': 'No_Version',
@@ -91,8 +89,6 @@ const createNewCsv = (inputFilePath, outputFilePath) => {
         { id: 'Components', title: 'Components' },
         { id: 'Priority', title: 'Priority' },
         { id: 'Description', title: 'Description' },
-        { id: 'Assignee', title: 'Assignee' },
-        { id: 'Reporter', title: 'Reporter' },
         { id: 'Status', title: 'Status' },
         { id: 'Triage_Status', title: 'Triage_Status' },
         { id: 'Affects versions', title: 'Affects versions' },
@@ -123,27 +119,40 @@ function getComponentsData(row) {
 
 function mapComponent(alcComponent) {
   const mapping = {
-    'Web': 'WEB',
-    'Android': 'Android',
-    'iOS': 'iOS',
-    'Android TV': 'ATV',
-    'WebOS': 'HTML TV',
-    'Smart TV': 'HTML TV',
-    'Tizen': 'HTML TV',
-    'VIDAA': 'HTML TV',
-    'CP backend': 'Backend',
-    'DP Backend': 'DP Backend',
-    'EM': 'CMS',
-    // Other mappings...
+      'Web': 'WEB',
+      'Android': 'Android',
+      'iOS': 'iOS',
+      'Android TV': 'ATV',
+      'WebOS': 'HTML TV',
+      'Smart TV': 'HTML TV',
+      'Tizen': 'HTML TV',
+      'VIDAA': 'HTML TV',
+      'CP backend': 'Backend',
+      'DP Backend': 'DP Backend',
+      'EM': 'CMS',
+      'TV Stick': null, // <dont import>
+      'TA': null, // <dont import>
+      'EV': null, // <dont import>
+      'OPC': null, // <dont import>
+      'UES': null, // <dont import>
+      'AMS': null, // <dont import>
+      'Conviva': 'Throw Exception', // Throw exception
+      'AA': 'Throw Exception', // Throw exception
+      'Ads_Banner': 'Throw Exception', // Throw exception
+      'CSAI': 'Throw Exception', // Throw exception
+      'SSAI': 'Throw Exception', // Throw exception
+      'VOD Publishing': 'Throw Exception', // Throw exception
+      'Linear Publishing': 'Throw Exception' // Throw exception
   };
 
   const result = mapping[alcComponent];
+
   if (result === 'Throw Exception') {
-    throw new Error(`Exception: Check ALC jira for '${alcComponent}'`);
+      throw new Error(`Exception: Check ALC jira for '${alcComponent}' and manually map correct component.`);
   } else if (result === null) {
-    return null;
+      return null; // <dont import> condition
   } else {
-    return result;
+      return result;
   }
 }
 
