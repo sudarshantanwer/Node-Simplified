e3c941bc262cc0044c86444ca5de642fc4491005
Sudarshan Tanwar <sudarshantanwer@gmail.com>
2026-05-01 01:11:14 +0530

    Add health check endpoint to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Server is working 🚀");
+});
+
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
